73d16c479a0b71256c786f2d9cdda102b9503708
GerberAsturias <149099484+GerberAsturias@users.noreply.github.com>
2025-08-23 23:36:45 -0600

    Add files via upload

---
+++ EmpleadosPrueba/EmpleadosPrueba/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+namespace EmpleadosPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Conexion cn = new Conexion();
+
+        void insertarEmpleado()
+        {
+            string cadena = "INSERT INTO empleados (nombre_completo, puesto, departamento) VALUES ('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "')";
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insertarEmpleado();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Consulta consulta = new Consulta();
+            consulta.Show();
+        }
+    }
+}
